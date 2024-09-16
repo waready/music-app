f3e15d1e01e02d458675cb20a8b358b381ba5648
waready <elbarto119@hotmail.com>
2024-09-16 00:37:54 -0500

    datos

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,28 +1,129 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+  <q-page class="q-pa-md bg-dark text-white">
+    <!-- <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px"> -->
     <pre>{{ song }}</pre>
-    <q-footer elevated class="bg-white text-dark">
+
+
+    <div class="q-pa-md">
+      <q-list bordered>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon color="primary" name="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Icon as avatar</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar color="teal" text-color="white" icon="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Avatar-type icon</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar rounded color="purple" text-color="white" icon="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Rounded avatar-type icon</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar color="primary" text-color="white">
+              R
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>Letter avatar-type</q-item-section>
+        </q-item>
+
+        <q-separator />
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Image avatar</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar square>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Image square avatar</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar rounded>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Image rounded avatar</q-item-section>
+        </q-item>
+
+        <q-separator />
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar rounded>
+              <img src="https://cdn.quasar.dev/img/mountains.jpg">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>List item</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section thumbnail>
+            <img src="https://cdn.quasar.dev/img/mountains.jpg">
+          </q-item-section>
+          <q-item-section>List item</q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+
+    <q-footer elevated class="bg-primary text-white q-py-md">
       <q-toolbar>
         <q-toolbar-title>
-          <q-card>
-            <q-linear-progress :value="valor" max="1" color="pink" id="segundo" class="q-pb-sm" ref="segundo" />
-            <q-card-section class="row items-center no-wrap">
-              <div class="q-mr-md">
-                <div class="text-weight-bold">{{ song.title }}</div>
-                <div class="text-grey">{{ song.artist }}</div>
+          <q-card class="bg-primary text-white">
+            <q-linear-progress :value="valor" max="1" color="green-7" id="segundo" class="q-pb-sm" ref="segundo" />
+            <q-card-section class="row items-center">
+              <div class="col">
+                <div class="q-mr-md">
+                  <div class="text-weight-bold">{{ song.artist }}</div>
+                  <div class="text-grey ellipsis">{{ song.title }}</div>
+                </div>
+                <!-- Componente de Audio oculto -->
+                <audio id="audio-player" style="width: 100%;" controls controlsList="nodownload" ref="audio1"
+                  @timeupdate="updateProgress" @play="onPlay" v-show="false">
+                  <source :src="'http://localhost:3000/mp3/' + song.audioPath" type="audio/ogg">
+                  <source :src="'http://localhost:3000/mp3/' + song.audioPath" type="audio/mpeg">
+                  Your browser does not support the audio element.
+                </audio>
               </div>
-              <audio id="audio-player" style="width: 100%;" controls controlsList="nodownload" ref="audio1"
-                @timeupdate="updateProgress" @play="onPlay">
-                <source :src="'http://localhost:3000/mp3/' + song.audioPath" type="audio/ogg">
-                <source :src="'http://localhost:3000/mp3/' + song.audioPath" type="audio/mpeg">
-                Your browser does not support the audio element.
-              </audio>
+              <!-- Componente de Audio oculto -->
               <q-space />
-              <q-btn flat round icon="replay_circle_filled" @click="replay" />
+              <!-- <q-btn flat round icon="replay_circle_filled" @click="replay" /> -->
+
               <q-btn flat round icon="fast_rewind" @click="previous" />
               <q-btn flat round icon="pause" @click="togglePlayPause" />
               <q-btn flat round icon="fast_forward" @click="next" />
+              <div class="time-remaining">{{ formattedTimeRemaining }}</div>
+              <div class="volume-controls q-ml-md">
+                <q-btn flat round icon="volume_down" @click="decreaseVolume" />
+                <!-- <q-slider v-model="volume" color="green" /> -->
+                <q-linear-progress :value="volume" max="1" color="green-7" id="segundo" class="q-pb-sm" ref="segundo" />
+                <q-btn flat round icon="volume_up" @click="increaseVolume" />
+              </div>
+
             </q-card-section>
           </q-card>
         </q-toolbar-title>
@@ -56,6 +157,8 @@ export default defineComponent({
   data() {
     return {
       valor: 0,
+      volume: 0.5,
+      timeRemaining: 0,
       song: {
         title: '',
         artist: '',
@@ -154,6 +257,9 @@ export default defineComponent({
       const audio = this.$refs.audio1;
       if (audio) {
         this.valor = audio.currentTime / audio.duration;
+        this.timeRemaining = audio.duration - audio.currentTime;
+      } else {
+        this.valor = 0;
       }
     },
     fetchPlaylist() {
@@ -166,7 +272,7 @@ export default defineComponent({
     },
     handleDownload(e) {
       e.preventDefault();
-      fetch(`http://localhost:3000/download?url=${encodeURIComponent(this.url)}`)
+      fetch(`http://localhost:3000/api/download?url=${encodeURIComponent(this.url)}`)
         .then(response => response.json())
         .then(data => {
           if (data.error) {
@@ -201,11 +307,47 @@ export default defineComponent({
         audio.pause();
       }
       this.isSyncing = false;
+    },
+    decreaseVolume() {
+      const audio = this.$refs.audio1;
+      console.log(audio.volume)
+      if (audio && audio.volume > 0) {
+        audio.volume = Math.max(0, audio.volume - 0.1); // Reduce el volumen en 0.1
+        this.volume = audio.volume
+      }
+    },
+    increaseVolume() {
+      const audio = this.$refs.audio1;
+      console.log(audio.volume)
+      if (audio && audio.volume < 1) {
+        audio.volume = Math.min(1, audio.volume + 0.1); // Aumenta el volumen en 0.1
+        this.volume = audio.volume
+      }
+    },
+
+  },
+  computed: {
+    // Formatear el tiempo restante a minutos y segundos
+    formattedTimeRemaining() {
+      const minutes = Math.floor(this.timeRemaining / 60);
+      const seconds = Math.floor(this.timeRemaining % 60).toString().padStart(2, '0');
+      return `${minutes}:${seconds}`;
     }
-  }
+  },
 });
 </script>
 
 <style>
-/* Agrega tus estilos aquí */
+.volume-controls {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 150px;
+  /* Ajusta el ancho del slider según necesites */
+}
+
+.time-remaining {
+  margin-left: 10px;
+  font-weight: bold;
+}
 </style>
